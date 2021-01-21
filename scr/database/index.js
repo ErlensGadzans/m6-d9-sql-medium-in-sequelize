68f06e9d1845d301328153c6c3265fadcf486379
ErlensGadzans <erlens.gadzans@users.noreply.github.com>
2021-01-21 17:42:48 +0200

    Connection has been established successfully.

---
+++ scr/database/index.js
@@ -0,0 +1,20 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(
+  process.env.PGDATABASE,
+  process.env.PGUSER,
+  process.env.PGPASSWORD,
+  {
+    host: process.env.PGHOST,
+    dialect: "postgres",
+  }
+);
+
+try {
+  sequelize.authenticate();
+  console.log("Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}
+
+module.exports = sequelize;
